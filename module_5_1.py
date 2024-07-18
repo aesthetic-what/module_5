daa6940608beac43c2a3781993299d7a3baf4b78
aesthetic-what <ladnoladno477@gmail.com>
2024-07-18 20:07:17 +0500

    module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -1,7 +1,7 @@
 class House:
     def __init__(self, name, number_of_floors):
         self.name = name
-        self.number_of_floors = int(number_of_floors)
+        self.number_of_floors = number_of_floors
 
     def go_to(self, new_floor):
         for i in range(1, self.number_of_floors):
